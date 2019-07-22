7efab454b932261c5c7463f983cd0af59d155071
Taimaru Provensal <taimaru@gmail.com>
2019-07-21 21:17:33 -0700

    fixed issue where output was correct but rspec tests weren't passing

--- module3/http_assn/module3_1_assignment.rb
+++ module3/http_assn/module3_1_assignment.rb
@@ -3,12 +3,13 @@ require 'httparty'
 class Recipe
   include HTTParty
 
-  base_uri 'http://food2fork.com/api'
+
+  base_uri "http://food2fork.com/api"
   default_params key: ENV['FOOD2FORK_KEY']
   format :json
 
   def self.for(keyword)
-    get("/search", query: { query: keyword }["recipes"])
+    get("/search", query: { q: keyword })["recipes"]
   end
 
 end
